22c51affa5f1233e42b3f05bb47c4bbf5ecc799e
Parth Panara <parthpanara91@gmail.com>
2026-02-03 00:27:41 +0530

    Initial project setup: Password Generator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Passsword Generator</h1>
+    </>
+  )
+}
+
+export default App
